9a46a3b786ef38544825d177e87e8d614645d932
only <1067341470@qq.com>
2019-07-03 20:13:13 +0800

    shi jian

---
+++ database/migrations/2019_07_03_200057_create_table_order.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableOrder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->string('id')->index();
+            $table->string('name');
+            $table->timestamp('ordernum')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('order');
+    }
+}
